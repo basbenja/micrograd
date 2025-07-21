0772dec67a47ec00c20a34642093a8e1a971277c
Benjamín Bas Peralta <bbas@entelai.com>
2025-07-21 19:32:11 -0300

    revised definition of derivativ and its numerical approximation

---
+++ Value.py
@@ -0,0 +1,27 @@
+class Value:
+    def __init__(self, data: float, _children: tuple = (), _op: str = '', label: str = '') -> "Value":
+        """
+        Parameters:
+            data (float): the actual value wrapped around Value.
+            _children (float): placeholder for the previous values that generated this Value.
+            _op (str): the operation applied between the children that gave as a result this Value.
+        """
+        self.data = data
+        self._prev = set(_children)
+        self._op = _op
+        self.label = label
+        # Maintaints the derivative of the loss function with respect to this variable
+        # 0 means no effect. This means that at initialization, we are assuming that
+        # no variable affects the output
+        self.grad = 0
+
+    def __repr__(self) -> str:
+        return f"Value(data={self.data})"
+
+    def __add__(self, other: "Value") -> "Value":
+        out = Value(self.data + other.data, (self, other), '+')
+        return out
+
+    def __mul__(self, other: "Value") -> "Value":
+        out = Value(self.data * other.data, (self, other), '*')
+        return out

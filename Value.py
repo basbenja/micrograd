e46173065dcb7d30b5d1378414483622a2942ecc
Benjamín Bas Peralta <bbas@entelai.com>
2025-07-23 19:57:42 -0300

    first implementation of (semi) automatic backpropagation (added backward attribute to the Value class)

--- Value.py
+++ Value.py
@@ -12,20 +12,42 @@ class Value:
         self._prev = set(_children)
         self._op = _op
         self.label = label
-        # Maintaints the derivative of the loss function with respect to this variable
+
+        # Maintains the derivative of the loss function with respect to this variable
         # 0 means no effect. This means that at initialization, we are assuming that
         # no variable affects the output
         self.grad = 0
+
+        # It is a function that is going to apply the chain rule for a certain value
+        # We are going to store how we are going to change the outputs gradients
+        # with the inputs gradients
+        self._backward = lambda: None
 
     def __repr__(self) -> str:
         return f"Value(data={self.data})"
 
     def __add__(self, other: "Value") -> "Value":
         out = Value(self.data + other.data, (self, other), '+')
+
+        # Remember: when a value is the sum of two values, the local derivative is 1
+        # It just backpropagates the previous gradient
+        def _backward():
+            self.grad = 1.0 * out.grad
+            other.grad = 1.0 * out.grad
+        out._backward = _backward
+
         return out
 
     def __mul__(self, other: "Value") -> "Value":
         out = Value(self.data * other.data, (self, other), '*')
+
+        # Remember: when a value is the product of two values, the local derivative of
+        # each operand is the other operand
+        def _backward():
+            self.grad = other.data * out.grad
+            other.grad = self.data * out.grad
+        out._backward = _backward
+
         return out
 
     def tanh(self):
@@ -34,4 +56,9 @@ class Value:
         x = self.data
         t = (math.exp(2*x) - 1) / (math.exp(2*x) + 1)
         out = Value(t, (self, ), 'tanh')
+
+        def _backward():
+            self.grad = (1 - t**2) * out.grad
+        out._backward = _backward
+
         return out
